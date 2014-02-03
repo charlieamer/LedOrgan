ed6ab90956dbdbcaa4a660904a3dbdee00436c9b
charlieamer <amer_zavlan@live.com>
2014-02-03 20:47:46 +0100

    Initial commit, contains test source code for atmega328p

---
+++ ATMEGA328P/main.c
@@ -0,0 +1,28 @@
+//these are the include files. They are outside the project folder
+#include <avr/io.h>
+#include <avr/delay.h>
+
+void delay_ms(uint16_t ms) {
+		//while loop that creates a delay for the duration of the millisecond countdown
+        while ( ms ) 
+        {
+                _delay_ms(1);
+                ms--;
+        }
+}
+
+int main (void)
+{
+	//Set PORTC to all outputs
+	DDRC = 0xFF;
+	//create an infinite loop
+	while(1) {
+		PORTC |=(1<<0);
+		//PAUSE 250 miliseconds
+		delay_ms(250);
+		//turns C0 LOW
+		PORTC &= ~(1 << 0);
+		//PAUSE 250 miliseconds
+		delay_ms(250);
+	};
+}
